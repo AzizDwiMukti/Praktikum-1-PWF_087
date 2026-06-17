0196ce6ff8faf111ca8de6a33fe526141158596d
AzizDwiMukti <azizdwimukti@gmail.com>
2026-06-17 09:18:05 +0700

    pw

---
+++ pertemuan-1/resources/views/about.blade.php
@@ -0,0 +1,20 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-200">
+            About
+        </h2>
+    </x-slot>
+
+    <div class="py-10">
+        <div class="max-w-4xl mx-auto">
+            <div class="bg-gray-800 p-6 rounded-lg text-white">
+
+                <p><b>Nama:</b> Azis Dwi Mukti</p>
+                <p><b>NIM:</b> 20230140087</p>
+                <p><b>Prodi:</b> Informatika</p>
+                <p><b>Hobi:</b> Ngoding, Gaming</p>
+
+            </div>
+        </div>
+    </div>
+</x-app-layout>
